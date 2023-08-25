48f49ffc4e28f022860bf8cf5c166b9d4bcee404
nick-sohl <90460746+nick-sohl@users.noreply.github.com>
2023-08-25 10:54:28 +0200

    Add files via upload

---
+++ card_glassmorphism/js/index.js
@@ -0,0 +1,44 @@
+// let firstName = "Nick"
+// let lastName = "Sohl"
+// let fullName = firstName + " " + lastName
+
+// console.log(fullName)
+
+
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// function greetLinda() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// greetLinda()
+
+
+// let myPoints = 3
+
+// function add3Points() {
+//     myPoints += 3
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+
+// function remove1Point() {
+//     myPoints -= 1
+// }
+
+// remove1Point()
+// remove1Point()
+
+// console.log(myPoints)
+
+
+let error = document.getElementById("error")
+
+let errorMessage = "Something went wrong, please try again."
+
+function purchase_error() {
+    error.textContent = errorMessage
+}
